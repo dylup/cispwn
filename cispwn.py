1b55132e501fac13e8d8f81dd888db55952a88b5
Daidis <zchoiberg@gmail.com>
2016-10-05 13:30:16 -0500

    Added tftp_setup and copy_config commands

--- cispwn.py
+++ cispwn.py
@@ -5,20 +5,22 @@ import sys
 import getopt
 
 version = 'Version 0.1'
+tftp=0
+copied=0
 
 #ser = serial.Serial('/dev/ttyUSB0') # open serial port
 #print (ser.name) # check which port was really used
 #ser.close() # close
 #test
-#Upon power cycling, loops the command until the router boots with a blank config.
+#Upon power cycling, loops the command until the router boots with a blank config. It then enables higher priviledges for further commands to be run.
 def rommon(console):
 	rom=false
 	while rom==false:
 		prompt=read_serial(console)
 		#Checks if the config has already been loaded
 		if "Press RETURN" in prompt:
-			print "Power cycle the router again"
-			time.sleep(5)
+			print "Power cycle the router again. This script will wait for 10 seconds and loop again."
+			time.sleep(10)
 		#Sends the Ctrl+C to stop the boot and enter rommon
 		elif "Self decompressing the image" in prompt:
 			send_command(console,'\x03')
@@ -28,17 +30,50 @@ def rommon(console):
 			send_command(console,'confreg 2042')
 			send_command(console,'boot')
 			print "Booting"
+			time.sleep(20)
+			send_command(console,'no')
+			send_command(console,'')
+			send_command(console,'enable')
 
-#def copy_config(console):
-	#tftp the config file
+#sets IP settings of the router's interface
+def tftp_setup(console):
+	send_command(console,'config t')
+	send_command(console,'do show ip int brief')
+	prompt=read_serial(console)
+	#Checks whether a Gigabit interface is available, if not, it defaults to the first FastEthernet interface.
+	print "Setting interface options"
+	if "GigabitEthernet0/0" in prompt:
+		send_command(console,'interface Gig0/0')
+	else:
+		send_command(console,'interface Fa0/0')
+	send_command(console,'no shut')
+	send_command(console,'ip addr 192.168.1.1 255.255.255.0')
+	send_command(console,'\x03')
+	send_command(console,'')
+	print "Interface options set"
+	tftp=1
 
 
-#def decrypt_passwords(console):
-	#copy_config() and parse the file for cisco 7 passwords, then crack them and display them in plaintext
+def copy_config(console):
+	#tftp the config file to the host machine for further commands
+	if(tftp==0):
+		tftp_setup(console)
+	send_command(console,'copy startup-config tftp:')
+	send_command(console,'192.168.1.2')
+	send_command(console,'')
+	copied=1
 
 
-#def randomize_passwords(console):
-	#copy_config() parse the file for passwords, replace them with random encrypted passwords
+def decrypt_passwords(console):
+	if(copied==0):
+		copy_config() 
+		#and parse the file for cisco 7 passwords, then crack them and display them in plaintext
+
+
+def randomize_passwords(console):
+	if(copied==0):
+		copy_config()
+		#parse the file for passwords, replace them with random encrypted passwords
 
 
 #def delete_config(console):
@@ -52,7 +87,7 @@ def read_serial(console):
 		return console.read(data_bytes)
 	else:
 		return ""
-
+#might remove reading portion for less output to screen
 def send_command(console,cmd=''):
 	#Sends a command to the router and returns the bytes in waiting as output.
 	console.write(cmd+'\r\n')
